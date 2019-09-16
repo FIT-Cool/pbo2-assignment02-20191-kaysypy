20b701e803644f5b838c911d87eab2103d2639d3
kaysypy <55116639+kaysypy@users.noreply.github.com>
2019-09-16 21:41:43 +0700

    Add files via upload

--- src/com/kevin/controller/MainFormController.java
+++ src/com/kevin/controller/MainFormController.java
@@ -2,29 +2,25 @@ package com.kevin.controller;
 
 import com.kevin.entity.Category;
 import com.kevin.entity.Item;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainFormController implements Initializable {
+    public Button btnUp;
     @FXML
     private TextField isiNama;
     @FXML
     private TextField isiHarga;
     @FXML
-    private ComboBox isiCat;
-    @FXML
+
     private TextField masCat;
     @FXML
     private TableColumn<Item,String>  colNam;
@@ -38,23 +34,34 @@ public class MainFormController implements Initializable {
     private ObservableList<Item> items;
     private ObservableList<Category> categories;
 
+
+    private ComboBox isiCat;
     @FXML
+
+
     private void saveCat(ActionEvent actionEvent) {
-        Category c=new Category();
-        c.setName(masCat.getText().trim());
-        categories.add(c);
+		isiCat.getValue().setItems(masCat.getText());
     }
 
     @FXML
     private void actionSave(ActionEvent actionEvent) {
+		Item i=new Item();
+        i.setName(isiNama.getText().trim());
+        i.setPrice(isiHarga.getText().trim());
+		i.setCategory(isiCat.getText().trim());
+        items.add(i);
     }
 
     @FXML
     private void actionReset(ActionEvent actionEvent) {
+		isiNama.clear();
+		isiHarga.clear();
+		isiCat.clear();
     }
 
     @FXML
     private void actionUpdate(ActionEvent actionEvent) {
+        btnUp.setDisable(false);
     }
 
     @FXML
@@ -63,24 +70,29 @@ public class MainFormController implements Initializable {
         //txtName.setText(f.getNama());
     }
 
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        items= FXCollections.observableArrayList();
-        itemDepartment.setItems(items);
-        colNam.setCellValueFactory(data ->{
-            Item i=data.getValue();
-            return new SimpleStringProperty(i.getName());
-        });
+			items= FXCollections.observableArrayList();
+			itemDepartment.setItems(items);
+			colNam.setCellValueFactory(data ->{
+				Item i=data.getValue();
+				return new SimpleStringProperty(i.getName());
+			});
 
-        colPrice.setCellValueFactory(data ->{
-            Item i=data.getValue();
-            return new SimpleStringProperty(String.valueOf(i.getPrice()));
-        });
+			colPrice.setCellValueFactory(data ->{
+				Item i=data.getValue();
+				return new SimpleStringProperty(String.valueOf(i.getPrice()));
+			});
 
+        };
 
+    public ObservableList<Category> getCategories() {
+        return categories;
+    }
 
-
-
-
+    public void setCategories(ObservableList<Category> categories) {
+        this.categories = categories;
     }
 }
+
